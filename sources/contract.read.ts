460c2a8e9b4abb23422c0848634f92db63bade2a
Howard Peng <howard.pen9@gmail.com>
2023-08-23 11:11:11 +0800

    Add the read file example

---
+++ sources/contract.read.ts
@@ -0,0 +1,37 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { Address, contractAddress } from "ton";
+import { SampleTactContract } from "./output/sample_SampleTactContract";
+import { prepareTactDeployment } from "@tact-lang/deployer";
+
+(async () => {
+
+    // Parameters
+    let testnet = true;
+    let packageName = 'sample_SampleTactContract.pkg';
+    let owner = Address.parse('kQBM7QssP28PhrctDOyd47_zpFfDiQvv5V9iXizNopb1d2LB');
+    let init = await SampleTactContract.init(owner);
+
+    // Load required data
+    let address = contractAddress(0, init);
+    let data = init.data.toBoc();
+    let pkg = fs.readFileSync(path.resolve(__dirname, 'output', packageName));
+
+    // Prepareing
+    console.log('Uploading package...');
+
+    // Deploying
+    console.log("============================================================================================");
+    console.log('Contract Address')
+    console.log("============================================================================================");
+    console.log();
+    console.log(address.toString({ testOnly: testnet }));
+    console.log();
+    console.log("============================================================================================");
+    console.log('Please, follow deployment link')
+    console.log("============================================================================================");
+    console.log();
+    console.log(prepare);
+    console.log();
+    console.log("============================================================================================");
+})();
