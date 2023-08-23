cf3f79dd65c6aeb0ca4d3995ae041563660a7b36
Howard Peng <howard.pen9@gmail.com>
2023-08-23 11:26:36 +0800

    updates

--- sources/contract.deploy.ts
+++ sources/contract.deploy.ts
@@ -1,35 +1,35 @@
-import * as fs from 'fs';
-import * as path from 'path';
+import * as fs from "fs";
+import * as path from "path";
 import { Address, contractAddress } from "ton";
 import { SampleTactContract } from "./output/sample_SampleTactContract";
 import { prepareTactDeployment } from "@tact-lang/deployer";
 
 (async () => {
-
     // Parameters
     let testnet = true;
-    let packageName = 'sample_SampleTactContract.pkg';
-    let owner = Address.parse('kQBM7QssP28PhrctDOyd47_zpFfDiQvv5V9iXizNopb1d2LB');
+    let packageName = "sample_SampleTactContract.pkg";
+    // let owner = Address.parse('kQBM7QssP28PhrctDOyd47_zpFfDiQvv5V9iXizNopb1d2LB');
+    let owner = Address.parse("kQAWGf7Kbc9-OrZJc0o2EUX-NaY23BDBv6XqsZhE_QBX8CVp");
     let init = await SampleTactContract.init(owner);
 
     // Load required data
     let address = contractAddress(0, init);
     let data = init.data.toBoc();
-    let pkg = fs.readFileSync(path.resolve(__dirname, 'output', packageName));
+    let pkg = fs.readFileSync(path.resolve(__dirname, "output", packageName));
 
     // Prepareing
-    console.log('Uploading package...');
+    console.log("Uploading package...");
     let prepare = await prepareTactDeployment({ pkg, data, testnet });
 
     // Deploying
     console.log("============================================================================================");
-    console.log('Contract Address')
+    console.log("Contract Address");
     console.log("============================================================================================");
     console.log();
     console.log(address.toString({ testOnly: testnet }));
     console.log();
     console.log("============================================================================================");
-    console.log('Please, follow deployment link')
+    console.log("Please, follow deployment link");
     console.log("============================================================================================");
     console.log();
     console.log(prepare);
--- sources/contract.read.ts
+++ sources/contract.read.ts
@@ -1,37 +1,27 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import { Address, contractAddress } from "ton";
+import * as fs from "fs";
+import * as path from "path";
+import { Address, contractAddress, TonClient4 } from "ton";
 import { SampleTactContract } from "./output/sample_SampleTactContract";
 import { prepareTactDeployment } from "@tact-lang/deployer";
 
 (async () => {
+    const client = new TonClient4({
+        endpoint: "https://sandbox-v4.tonhubapi.com", // 🔴 Test-net API endpoint
+    });
 
     // Parameters
     let testnet = true;
-    let packageName = 'sample_SampleTactContract.pkg';
-    let owner = Address.parse('kQBM7QssP28PhrctDOyd47_zpFfDiQvv5V9iXizNopb1d2LB');
+    let packageName = "sample_SampleTactContract.pkg";
+    let owner = Address.parse("kQBM7QssP28PhrctDOyd47_zpFfDiQvv5V9iXizNopb1d2LB");
     let init = await SampleTactContract.init(owner);
-
-    // Load required data
-    let address = contractAddress(0, init);
-    let data = init.data.toBoc();
-    let pkg = fs.readFileSync(path.resolve(__dirname, 'output', packageName));
+    let contract_address = contractAddress(0, init);
 
     // Prepareing
-    console.log('Uploading package...');
+    console.log("Reading Contract Info...");
+    console.log(contract_address);
 
-    // Deploying
-    console.log("============================================================================================");
-    console.log('Contract Address')
-    console.log("============================================================================================");
-    console.log();
-    console.log(address.toString({ testOnly: testnet }));
-    console.log();
-    console.log("============================================================================================");
-    console.log('Please, follow deployment link')
-    console.log("============================================================================================");
-    console.log();
-    console.log(prepare);
-    console.log();
-    console.log("============================================================================================");
+    // Input the contract address
+    let contract = await SampleTactContract.fromAddress(contract_address);
+    let contract_open = await client.open(contract);
+    console.log("Counter Value: " + (await contract_open.getCounter()));
 })();
