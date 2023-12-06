cc378d96b56a71b2c328bf790277210496fec146
Anton Trunov <anton@ton.org>
2023-12-06 14:50:14 +0400

    Update to @ton/core deps

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
-  snapshotSerializers: ["ton-jest/serializers"],
+  snapshotSerializers: ["@tact-lang/ton-jest/serializers"],
   globalSetup: './jest.setup.js',
   globalTeardown: './jest.teardown.js',
 };
