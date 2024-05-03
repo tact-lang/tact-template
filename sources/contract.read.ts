800f270162361dd3c55c6f5eba037b7e8ba30663
rustam87 <abrus1987@gmail.com>
2024-05-03 19:52:34 +0300

    fix: issue with contract reader (#12)

--- sources/contract.read.ts
+++ sources/contract.read.ts
@@ -1,8 +1,6 @@
-import * as fs from "fs";
-import * as path from "path";
-import { Address, contractAddress, TonClient4 } from "ton";
+import { Address, contractAddress} from "@ton/core";
+import { TonClient4 } from "@ton/ton";
 import { SampleTactContract } from "./output/sample_SampleTactContract";
-import { prepareTactDeployment } from "@tact-lang/deployer";
 
 (async () => {
     const client = new TonClient4({
@@ -10,8 +8,6 @@ import { prepareTactDeployment } from "@tact-lang/deployer";
     });
 
     // Parameters
-    let testnet = true;
-    let packageName = "sample_SampleTactContract.pkg";
     let owner = Address.parse("kQBM7QssP28PhrctDOyd47_zpFfDiQvv5V9iXizNopb1d2LB");
     let init = await SampleTactContract.init(owner);
     let contract_address = contractAddress(0, init);
