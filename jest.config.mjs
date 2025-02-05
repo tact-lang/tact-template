befc2e2c5a2c72ba9990aef96861bcdd647f18f6
skulidropek <legovlad818@gmail.com>
2025-02-05 18:25:39 +0200

    Made Jest files use ES modules

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,8 +1,8 @@
-module.exports = {
+export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
   snapshotSerializers: ["@tact-lang/ton-jest/serializers"],
-  globalSetup: './jest.setup.js',
-  globalTeardown: './jest.teardown.js',
+  globalSetup: './jest.setup.mjs',
+  globalTeardown: './jest.teardown.mjs',
 };
